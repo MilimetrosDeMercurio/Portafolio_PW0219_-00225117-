0614c43ccf1489b24fdb3562e33afa85fb4f4910
UCA <riveraalejandro1999>
2019-09-25 11:05:38 -0600

    Ejercicio7.js

--- Portafolio/Laboratorios/Laboratorio5/Ejercicio6.js
+++ Portafolio/Laboratorios/Laboratorio5/Ejercicio6.js
@@ -1,6 +1,18 @@
 /*Ordenamiento de array: Crear una función en JavaScript que ordene los elementos de un Array en orden ascendente. */
 
-function Ordenamiento(listaNumero){
-    
+function ordenar(lista){
+    for(var i = 0; i<lista.length; i++){
+        for(var j= 0; j<lista.length -1; j++){
+            if(lista[j+1]<lista[j]){
+                var aux = lista[j+1];
+                lista[j+1] = lista[j]
+                lista[j] = aux;
+            }
+        }
+    }
+
+    return lista;
 
 }
+
+console.log(ordenar([216546,21,56,0,35,8]));
